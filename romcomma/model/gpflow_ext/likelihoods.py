d0f7e60d48db9c4fe2a4258d0779217bda29b629
miltonrg <r.a.milton@sheffield.ac.uk>
2021-10-12 18:48:19 +0100

    Updated Licenses

--- romcomma/model/gpflow_ext/likelihoods.py
+++ romcomma/model/gpflow_ext/likelihoods.py
@@ -1,34 +1,36 @@
-# BSD 3-Clause License
-#
-# Copyright (c) 2019-2022, Robert A. Milton
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#  BSD 3-Clause License.
+# 
+#  Copyright (c) 2019-2021 Robert A. Milton. All rights reserved.
+# 
+#  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+#  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 
+#  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+# 
+#  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+#     software without specific prior written permission.
+# 
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+#  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """ Contains extensions to gpflow.likelihoods."""
 
 from __future__ import annotations
 
+from gpflow_ext.base import Covariance
 
 import gpflow as gf
 from gpflow.base import Parameter
 import tensorflow as tf
+import numpy as np
 
-class Likelihood(gf.likelihoods.QuadratureLikelihood):
+class MultivariateGaussian(gf.likelihoods.QuadratureLikelihood):
     """ A non-diagonal, multivariate likelihood, extending gpflow. The code is the multivariate version of gf.likelihoods.Gaussian.
 
     The Gaussian likelihood is appropriate where uncertainties associated with
@@ -39,60 +41,36 @@ class Likelihood(gf.likelihoods.QuadratureLikelihood):
     optimization process. A lower bound of 1e-6 is therefore imposed on the
     likelihood variance by default.
     """
+    @property
+    def covariance(self):
+        """ A gpflow_ext.base.Covariance, which is the covariance matrix of the likelihood distribution."""
+        return self._covariance
 
-    DEFAULT_VARIANCE_LOWER_BOUND = 1e-6
-
-    def __init__(self, variance_cho_diag: Sequence[float], variance_cho_off_diag: Sequence[float],
-                 variance_cho_diag_lower_bound=DEFAULT_VARIANCE_LOWER_BOUND, **kwargs):
+    def __init__(self, covariance, **kwargs):
         """ Constructor, which passes the Cholesky decomposition of the variance matrix.
 
         Args:
-            variance_cho_diag: The diagonal elements of the noise_variance Cholesky matrix, a sequence of length L.
-            variance_cho_off_diag: The off-diagonal elements of the noise_variance Cholesky matrix, a sequence of length L(L-1)/2.
-            variance_cho_diag_lower_bound: The lower bound for the variance diagonal.
+            covariance: The covariance matrix of the likelihood, expressed in tensorflow or numpy. Is checked for symmetry and positive definiteness.
             **kwargs: Keyword arguments forwarded to :class:`Likelihood`.
         """
-        self.__L = len(variance_cho_diag)
-        if len(variance_cho_off_diag) != (expected := self.__L * (self.__L - 1) / 2):
-            raise IndexError(f'len(noise_variance_cho_off_diag) = {len(variance_cho_off_diag)} instead of {expected} for L={self.__L}.')
-        super().__init__(latent_dim=self.__L, observation_dim=self.__L, **kwargs)
-        if min(variance_cho_diag) <= variance_cho_diag_lower_bound:
-            raise ValueError(f'The variance_cho_diag of the Gaussian Likelihood must be strictly greater than {variance_cho_diag_lower_bound}')
-
-        self.__variance_cho_diag = Parameter(variance_cho_diag, transform=gf.utilities.positive(lower=variance_cho_diag_lower_bound))
-        self.__variance_cho_off_diag = Parameter(variance_cho_off_diag)
-        self.__variance_cho_diag = self.__variance_cho_diag
-        self.__variance_cho_off_diag = self.__variance_cho_off_diag
-        self.__splits = [0]
-        for i in range(1, self.__L):
-            self.__splits.append(self.__splits[-1] + i)
-
-    @property
-    def variance_cho(self):
-        result = tf.RaggedTensor.from_row_splits(self.__variance_cho_off_diag.numpy(), self.__splits)
-        result = result.to_tensor(default_value=0, shape=(self.__L - 1, self.__L))
-        result = tf.concat([tf.zeros(0, self.__L), result])
-        return tf.linalg.set_diag(result, self.__variance_cho_diag)
+        self._covariance = Covariance(covariance)
+        super().__init__(latent_dim=self._covariance.shape, observation_dim=self._covariance.shape)
 
     def _log_prob(self, F, Y):
-        return logdensities.gaussian(Y, F, self.variance)
+        return gf.logdensities.multivariate_normal(Y, F, self._covariance.cholesky)
 
     def _conditional_mean(self, F):  # pylint: disable=R0201
         return tf.identity(F)
 
     def _conditional_variance(self, F):
-        return tf.fill(tf.shape(F), tf.squeeze(self.variance))
+        return self._covariance.value
 
     def _predict_mean_and_var(self, Fmu, Fvar):
-        return tf.identity(Fmu), Fvar + self.variance
+        return tf.identity(Fmu), Fvar + self._covariance.value
 
     def _predict_log_density(self, Fmu, Fvar, Y):
-        return tf.reduce_sum(logdensities.gaussian(Y, Fmu, Fvar + self.variance), axis=-1)
+        return tf.reduce_sum(gf.logdensities.multivariate_normal(Y, Fmu, tf.linalg.cholesky(Fvar + self._covariance.value)), axis=-1)
 
     def _variational_expectations(self, Fmu, Fvar, Y):
-        return tf.reduce_sum(
-            -0.5 * np.log(2 * np.pi)
-            - 0.5 * tf.math.log(self.variance)
-            - 0.5 * ((Y - Fmu) ** 2 + Fvar) / self.variance,
-            axis=-1,
-        )
+        tr = tf.linalg.cholesky_solve(self._covariance.cholesky, Fvar)
+        return self._log_prob(Fmu, Y) - 0.5 * tf.linalg.trace(tr)
