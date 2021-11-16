3a4a278b4e857b09aeb4ce398ad189f1cdfc46e4
Robert Milton <45204867+miltonra@users.noreply.github.com>
2021-11-16 00:59:31 +0000

    And again (+5 squashed commit)
    Squashed commit:
    
    [4210da7] try again
    
    [56fcdd9] another try with docs
    
    [bcd8d65] still trying
    
    [da5c481] again
    
    [52ea540] doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'm2r2'
+              'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,5 +55,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-source_suffix = ['.rst', '.md']
